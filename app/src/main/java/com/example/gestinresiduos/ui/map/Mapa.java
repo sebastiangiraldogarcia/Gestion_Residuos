a6615db2bd96cd2e66283e1c07a7043540fca27b
sebastiangiraldogarcia <sebastian.giraldo_g@uao.edu.co>
2020-06-18 00:41:55 -0500

    map

--- app/src/main/java/com/example/gestinresiduos/ui/map/Mapa.java
+++ app/src/main/java/com/example/gestinresiduos/ui/map/Mapa.java
@@ -3,13 +3,20 @@ package com.example.gestinresiduos.ui.map;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.graphics.BitmapFactory;
 import com.mapbox.mapboxsdk.Mapbox;
-import com.mapbox.mapboxsdk.annotations.MarkerOptions;
-import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
+
+
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.Point;
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,8 +28,19 @@ import android.view.ViewGroup;
 
 import com.example.gestinresiduos.R;
 
+import java.util.ArrayList;
+import java.util.List;
 
-public class Mapa extends Fragment {
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAllowOverlap;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconIgnorePlacement;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconOffset;
+
+/**
+ * Display {@link SymbolLayer} icons on the map.
+ */
+
+public class Mapa extends Fragment implements OnMapReadyCallback{
 
     private MapaViewModel mViewModel;
 
@@ -31,6 +49,10 @@ public class Mapa extends Fragment {
     public static Mapa newInstance() {
         return new Mapa();
     }
+
+    private static final String SOURCE_ID = "SOURCE_ID";
+    private static final String ICON_ID = "ICON_ID";
+    private static final String LAYER_ID = "LAYER_ID";
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -50,15 +72,41 @@ public class Mapa extends Fragment {
 
                     }
                     public void onMapReady (Mapbox mapbox) {
-                        MarkerOptions options = new MarkerOptions();
-                        options.title("Universidad Autónoma de Occidente");
-                        options.position(new LatLng(3.353693, -76.522872));
-                        mapboxMap.addMarker(options);
+                        //start
+                        List<Feature> symbolLayerIconFeatureList = new ArrayList<>();
+                        symbolLayerIconFeatureList.add(Feature.fromGeometry(
+                                Point.fromLngLat(-57.225365, -33.213144)));
+
+                        mapboxMap.setStyle(new Style.Builder().fromUri("mapbox://styles/mapbox/cjf4m44iw0uza2spb3q0a7s41")
+
+                                // Add the SymbolLayer icon image to the map style
+                                .withImage(ICON_ID, BitmapFactory.decodeResource(
+                                getActivity().getResources(), R.drawable.mapbox_marker_icon_default))
+
+                                // Adding a GeoJson source for the SymbolLayer icons.
+                                .withSource(new GeoJsonSource(SOURCE_ID,
+                                        FeatureCollection.fromFeatures(symbolLayerIconFeatureList)))
+
+
+                                // Adding the actual SymbolLayer to the map style. An offset is added that the bottom of the red
+                                // marker icon gets fixed to the coordinate, rather than the middle of the icon being fixed to
+                                // the coordinate point. This is offset is not always needed and is dependent on the image
+                                // that you use for the SymbolLayer icon.
+                                .withLayer(new SymbolLayer(LAYER_ID, SOURCE_ID)
+                                        .withProperties(iconImage(ICON_ID),
+                                                iconAllowOverlap(true),
+                                                iconOffset(new Float[] {0f, -9f}))
+                                ), new Style.OnStyleLoaded() {
+                            @Override
+                            public void onStyleLoaded(@NonNull Style style) {
+                            // Map is set up and the style has loaded. Now you can add additional data or make other map adjustments.
+                            }
+                        });                        
+                        //end
                     }
                 });
             }
         });
-
 
         return view;
     }
@@ -112,4 +160,8 @@ public class Mapa extends Fragment {
     }
 
 
+    @Override
+    public void onMapReady(@NonNull final MapboxMap mapboxMap) {
+        
+    }
 }

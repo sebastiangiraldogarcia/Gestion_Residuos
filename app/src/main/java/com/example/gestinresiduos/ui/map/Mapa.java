ab41e9ed3e2bbc82a2c2f2d493ac9e0730465d5e
kevinm <kevin.marmolejo@uao.edu.co>
2020-06-20 12:27:30 -0500

    mapa

--- app/src/main/java/com/example/gestinresiduos/ui/map/Mapa.java
+++ app/src/main/java/com/example/gestinresiduos/ui/map/Mapa.java
@@ -27,6 +27,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -72,16 +73,18 @@ public class Mapa extends Fragment implements OnMapReadyCallback{
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.mapa_fragment, container, false);
 
+        listaConte = new ArrayList<ubiConte>();
+        crud = new CRUDUbiConte();
+
+        new Mapa.LatLng(getActivity()).execute();
+
         mapView = view.findViewById(R.id.map1);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(@NonNull final MapboxMap mapboxMap) {//start
-
-                new LatLng(getActivity()).execute();
-
                 List<Feature> symbolLayerIconFeatureList = new ArrayList<>();
-                symbolLayerIconFeatureList.add(Feature.fromGeometry(Point.fromLngLat(-57.225365, -33.213144)));
+                symbolLayerIconFeatureList.add(Feature.fromGeometry(Point.fromLngLat(-76.522545, 3.353678)));
                 for (int i = 0; i < listaConte.size(); i++) {
                     symbolLayerIconFeatureList.add(Feature.fromGeometry(
                             Point.fromLngLat(Double.parseDouble(listaConte.get(i).getLongitud()),
@@ -191,7 +194,6 @@ public class Mapa extends Fragment implements OnMapReadyCallback{
                     @Override
                     public void run() {
                         // TODO Auto-generated method stub
-                        conte_existe();
                     }
                 });
             else
@@ -207,20 +209,22 @@ public class Mapa extends Fragment implements OnMapReadyCallback{
 
     private boolean conte_existe(){
         listaConte.clear();
+        Boolean r = false;
+        if(!crud.LatLng().equalsIgnoreCase("")){
+            String [] cargarDatos=crud.LatLng().split("/");
 
-        if(!crud.LatLng("a","a","a").equalsIgnoreCase("")){
-            String [] cargarDatos=crud.LatLng("a","a","a").split("/");
             for (int i = 0; i < cargarDatos.length; i++) {
                 String datosConte[]=cargarDatos[i].split("<br>");
-                ubiConte conte=new ubiConte();
+                ubiConte conte = new ubiConte();
                 conte.setUbicacion(datosConte[0]);
                 conte.setLatitud(datosConte[1]);
                 conte.setLongitud(datosConte[2]);
                 listaConte.add(conte);
             }
-            return true;
+            Log.d("lat", String.valueOf(listaConte.get(0).getLongitud()));
+            r = true;
         }
-        return false;
+        return r;
     }
 
 }
